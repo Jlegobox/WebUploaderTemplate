0d54456f56dce598377086033ea3e32547a7b5bf
jiangluchao_xencio <Xencio@123>
2021-02-15 22:28:43 +0800

    完善 前端展示内容 原因： 解决：

---
+++ src/main/java/com/Jlegobox/pojo/FileInfo.java
@@ -0,0 +1,97 @@
+package com.Jlegobox.pojo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author J
+ * @Email jlc_game123@163.com
+ * @Date 2021/2/14
+ * @Time 11:31
+ */
+public class FileInfo implements Serializable {
+    private String id;
+    private String originFileName;
+    private String fileSize;
+    private String lastModifiedDate;
+    private int chunks;
+    private int currentChunk;
+    private String MD5;
+    private String currentChunkMD5;
+    private Map<Integer,String> ChunkMD5 = new HashMap<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOriginFileName() {
+        return originFileName;
+    }
+
+    public void setOriginFileName(String originFileName) {
+        this.originFileName = originFileName;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(String lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public int getChunks() {
+        return chunks;
+    }
+
+    public void setChunks(int chunks) {
+        this.chunks = chunks;
+    }
+
+    public int getCurrentChunk() {
+        return currentChunk;
+    }
+
+    public void setCurrentChunk(int currentChunk) {
+        this.currentChunk = currentChunk;
+    }
+
+    public String getMD5() {
+        return MD5;
+    }
+
+    public void setMD5(String MD5) {
+        this.MD5 = MD5;
+    }
+
+    public String getCurrentChunkMD5() {
+        return currentChunkMD5;
+    }
+
+    public void setCurrentChunkMD5(String currentChunkMD5) {
+        this.currentChunkMD5 = currentChunkMD5;
+    }
+
+    public Map<Integer, String> getChunkMD5() {
+        return ChunkMD5;
+    }
+
+    public void setChunkMD5(Map<Integer, String> chunkMD5) {
+        ChunkMD5 = chunkMD5;
+    }
+}
